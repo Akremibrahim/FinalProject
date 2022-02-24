7969f55e49e5e3d6f6c20948d590613da659945b
Abshir Adan <test@MacBook-Pro.local>
2022-02-24 13:13:09 -0500

    LineGraph.js

--- src/map.js
+++ src/map.js
@@ -3,25 +3,18 @@ import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import "./Map.css";
 import { showDataOnMap } from "./util";
 
-function MyComponent() {
-  const map = useMap()
-  console.log('map center:', map.getCenter())
-  return null
-}
-
 function Map({ countries, casesType, center, zoom }) {
-  const map = useMap()
+  // const map = useMap()
   console.log(center)
   // const mapBounds = map.fitBounds(center) 
   return (
     <div className="map">
-      <MapContainer center={[50.5, 30.5]} zoom={13} >
-        <MyComponent/>
-        {/* <TileLayer
+      <MapContainer bounds={center} center={center} zoom={zoom}>
+        <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-        {showDataOnMap(countries, casesType)} */}
+        {showDataOnMap(countries, casesType)}
       </MapContainer>
     </div>
   );
